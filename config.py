1941571d965500e62ff9fb90b503aa97f9063ffc
Puguh Rismadi Ismail <puguh.rismadi@gmail.com>
2025-03-12 10:22:08 +0700

    delete pyc file and edit ip db

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:spasi2025@172.17.0.4/gallery_db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:spasi2025@192.168.7.126/gallery_db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
